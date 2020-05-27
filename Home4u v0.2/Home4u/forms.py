2db94e907cb8424cb75c9926ca9ba30f1b5a2c19
geotsiadis <33526951+geotsiadis@users.noreply.github.com>
2020-05-27 19:25:25 +0300

    Add files via upload

---
+++ Home4u v0.2/Home4u/forms.py
@@ -0,0 +1,104 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, NumberRange
+from Home4u.models import User, House, HouseSelector
+from wtforms.fields.html5 import DateField
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Κωδικός', validators=[DataRequired()])
+    confirm_password = PasswordField('Επαλήθευση Κωδικού',
+                                     validators=[DataRequired(), EqualTo('password')])
+    phone = IntegerField('Τηλέφωνο')
+    birth_date = DateField('Ημερομηνία Γέννησης', format='%Y-%m-%d')
+    firstname = StringField('Όνομα')
+    surname = StringField('Επίθετο')
+    sex = StringField('Φύλο')
+    submit = SubmitField('Εγγραφή')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Κωδικός', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Σύνδεση')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Ενημέρωση Εικόνας Προφίλ', validators=[FileAllowed(['jpg', 'png'])])
+    phone = IntegerField('Τηλέφωνο')
+    birth_date = DateField('Ημερομηνία Γέννησης', format='%Y-%m-%d')
+    firstname = StringField('Όνομα')
+    surname = StringField('Επίθετο')
+    sex = StringField('Φύλο')
+    submit = SubmitField('Ενημέρωση')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class ReviewForm(FlaskForm):
+    reviewer = StringField('Reviewer')
+    recipient = IntegerField('Recipient')
+    stars = IntegerField('Stars')
+    comments = StringField('Comments')
+
+
+class AddHouseForm(FlaskForm):
+    house_name = StringField('Όνομα Σπιτιού')
+    city = StringField('Πόλη')
+    postal_code = IntegerField('Ταχυδρομικός Κώδικας')
+    address = StringField('Διεύθυνση')
+    square_meters = IntegerField('Τετραγωνικά Μέτρα')
+    price = IntegerField('Τιμή')
+    house_type = StringField('Είδος Καταλύματος')
+    visitors = IntegerField('Επισκέπτες')
+    submit = SubmitField('Αποθήκευση')
+
+class UpdateHouseForm(FlaskForm):
+    house_name = StringField('Όνομα Σπιτιού')
+    city = StringField('Πόλη')
+    postal_code = IntegerField('Ταχυδρομικός Κώδικας')
+    address = StringField('Διεύθυνση')
+    square_meters = IntegerField('Τετραγωνικά Μέτρα')
+    price = IntegerField('Τιμή')
+    house_type = StringField('Είδος Καταλύματος')
+    visitors = IntegerField('Επισκέπτες')
+    picture = FileField('Ενημέρωση Εικόνας Προφίλ', validators=[FileAllowed(['jpg', 'png'])])
+    house_id = IntegerField('Id Σπιτιού')
+    submit = SubmitField('Αποθήκευση')
+
+
+class HouseSelectForm(FlaskForm):
+    house_id = IntegerField('Id Σπιτιού')
+    submit = SubmitField('Επεξεργασία')
